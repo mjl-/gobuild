b7d4f66e7bc3fcffc03bb068cd96d1796ae7a178
Mechiel Lukkien <mechiel@ueber.net>
2020-03-20 11:56:10 +0100

    parse html once at startup, not for every request

--- html.go
+++ html.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"html/template"
+	"log"
 	"net/http"
 )
 
@@ -33,4 +35,66 @@ func writeHTML(w http.ResponseWriter, content []byte) {
 	w.Write([]byte(lead))
 	w.Write(content)
 	w.Write([]byte(trail))
+}
+
+var (
+	homeTemplate, buildTemplate *template.Template
+)
+
+func init() {
+	var err error
+
+	buildTemplate, err = template.New("").Parse(`<p><a href="/">&lt; Home</a></p>
+<h1>{{ .Req.Mod }}@{{ .Req.Version }}/{{ .Req.Dir }}</h1>
+<h2>{{ .Req.Goos }}/{{ .Req.Goarch }} {{ .Req.Goversion }}</h2>
+<ul>
+	<li><a href="{{ .Sum }}">Download</a></li>
+	<li><a href="log">Log</a></li>
+	<li><a href="sha256">Sha256</a> ({{ .Sum }})</li>
+</ul>
+
+{{ $req := .Req }}
+<div style="width: 32%; display: inline-block; vertical-align: top">
+	<h2>Go versions</h2>
+{{ range .GoversionLinks }}	<div><a href="/x/{{ .Path }}">{{ .Goversion }}</a>{{ if .Available }} ✓{{ end }}</div>{{ end }}
+</div>
+
+<div style="width: 32%; display: inline-block; vertical-align: top">
+	<h2>Module versions</h2>
+	<div>TODO: fetch from module proxy/mirror/index</div>
+</div>
+
+<div style="width: 32%; display: inline-block; vertical-align: top">
+	<h2>Targets</h2>
+{{ range .TargetLinks }}	<div><a href="/x/{{ .Path }}">{{ .Goos }}/{{ .Goarch }}</a>{{ if .Available }} ✓{{ end }}</div>{{ end }}
+</div>
+`)
+	if err != nil {
+		log.Fatalf("parsing build html template: %v", err)
+	}
+
+	homeTemplate, err = template.New("").Parse(`<h1>gobuild - reproducible binaries with the go module proxy</h1>
+		<p>The Go team runs the <a href="https://proxy.golang.org/">Go module proxy</a>. This ensures code stays available, and you are likely to get the same code each time you fetch it. This helps you make reproducible builds. But you still have to build it yourself.</p>
+		<p>Gobuild actually compiles Go code available through the Go module proxy, and returns the binary.</p>
+
+		<h2>URLs</h2>
+		<p>Composition of URLs:</p>
+		<blockquote style="color:#666">https://gobuild.ueber.net/x/<span style="color:#111">&lt;goos&gt;</span>-<span style="color:#111">&lt;goarch&gt;</span>-<span style="color:#111">&lt;goversion&gt;</span>/<span style="color:#111">&lt;module&gt;</span>@<span style="color:#111">&lt;version&gt;</span>/<span style="color:#111">&lt;package&gt;</span>/</blockquote>
+		<p>Example:</p>
+		<blockquote><a href="/x/linux-amd64-go1.14/github.com/mjl-/sherpa@v0.6.0/cmd/sherpaclient/">https://gobuild.ueber.net/x/linux-amd64-go1.14/github.com/mjl-/sherpa@v0.6.0/cmd/sherpaclient/</a></blockquote>
+		<p>Opening this URL will either start a build, or show the results of an earlier build. The page shows links to download the binary, view the build output log file, the sha256 sum of the binary. You'll also see cross references to builds with different goversion, goos, goarch, and different versions of the module. You need not and cannot refresh a build, because they are reproducible.</p>
+
+		<h2>Recent builds</h2>
+		<ul>
+{{ range . }}			<li><a href="/x/{{ . }}">{{ . }}</a></li>{{ end }}
+		</ul>
+
+		<h2>Details</h2>
+		<p>Builds are created with CGO_ENABLED=0, -trimpath flag, and an zero buildid.</p>
+		<p>Only "go build" is run. No tests, no generate, no makefiles, etc.</p>
+		<p>Code is available at <a href="https://github.com/mjl-/gobuild">github.com/mjl-/gobuild</a>, under MIT-license.</p>
+`)
+	if err != nil {
+		log.Fatalf("parsing home html template: %v", err)
+	}
 }
